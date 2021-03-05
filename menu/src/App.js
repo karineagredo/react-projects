5078f759bd4bfaad82238e222f626267e32af579
Karine <karina.a182@gmail.com>
2021-03-06 09:34:14 +1100

    filter by all - menu

--- menu/src/App.js
+++ menu/src/App.js
@@ -7,19 +7,21 @@ import data from './data';
 const App = (props) => {
   const categories = new Set(data.map((item) => item.category));
   const [dishes, setDishes] = useState(data);
-  console.log(dishes);
-  const [menuOptions, setMenuOptions] = useState(categories);
+  const menuOptions = ['All', ...categories];
+  const filterItems = (category) => {
+    if (category === 'All') {
+      setDishes(data);
+      return;
+    }
+    setDishes(data.filter((item) => item.category === category));
+  };
+
   return (
     <>
       <main>
         <section className='menu section'></section>
         <Title></Title>
-        <Nav
-          categories={menuOptions}
-          filterClick={(category) =>
-            setDishes(data.filter((item) => item.category === category))
-          }
-        ></Nav>
+        <Nav categories={menuOptions} filterClick={filterItems}></Nav>
         <MenuRestaurant filteredDishes={dishes} />
       </main>
     </>
